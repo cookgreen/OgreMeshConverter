2ec16bac10ae185afb1f1ec8e03f2120e5e7cb48
DoDoCat <dell20091234@gmail.com>
2022-11-24 14:52:11 +0800

    Initial commit

---
+++ OgreMeshConverter/frmMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace OgreMeshConverter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOgreMesh = new System.Windows.Forms.TextBox();
+            this.btnBrowseMesh = new System.Windows.Forms.Button();
+            this.btnBrowseObj = new System.Windows.Forms.Button();
+            this.txtObjFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ogre3d Mesh File:";
+            // 
+            // txtOgreMesh
+            // 
+            this.txtOgreMesh.Location = new System.Drawing.Point(169, 15);
+            this.txtOgreMesh.Name = "txtOgreMesh";
+            this.txtOgreMesh.ReadOnly = true;
+            this.txtOgreMesh.Size = new System.Drawing.Size(359, 25);
+            this.txtOgreMesh.TabIndex = 1;
+            // 
+            // btnBrowseMesh
+            // 
+            this.btnBrowseMesh.Location = new System.Drawing.Point(534, 14);
+            this.btnBrowseMesh.Name = "btnBrowseMesh";
+            this.btnBrowseMesh.Size = new System.Drawing.Size(55, 26);
+            this.btnBrowseMesh.TabIndex = 2;
+            this.btnBrowseMesh.Text = "...";
+            this.btnBrowseMesh.UseVisualStyleBackColor = true;
+            this.btnBrowseMesh.Click += new System.EventHandler(this.btnBrowseMesh_Click);
+            // 
+            // btnBrowseObj
+            // 
+            this.btnBrowseObj.Location = new System.Drawing.Point(534, 57);
+            this.btnBrowseObj.Name = "btnBrowseObj";
+            this.btnBrowseObj.Size = new System.Drawing.Size(55, 26);
+            this.btnBrowseObj.TabIndex = 5;
+            this.btnBrowseObj.Text = "...";
+            this.btnBrowseObj.UseVisualStyleBackColor = true;
+            this.btnBrowseObj.Click += new System.EventHandler(this.btnBrowseObj_Click);
+            // 
+            // txtObjFile
+            // 
+            this.txtObjFile.Location = new System.Drawing.Point(169, 58);
+            this.txtObjFile.Name = "txtObjFile";
+            this.txtObjFile.ReadOnly = true;
+            this.txtObjFile.Size = new System.Drawing.Size(359, 25);
+            this.txtObjFile.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wavefont Obj File:";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(431, 98);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(158, 59);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 178);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnBrowseObj);
+            this.Controls.Add(this.txtObjFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowseMesh);
+            this.Controls.Add(this.txtOgreMesh);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OgreMeshConverter - [1.7]";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOgreMesh;
+        private System.Windows.Forms.Button btnBrowseMesh;
+        private System.Windows.Forms.Button btnBrowseObj;
+        private System.Windows.Forms.TextBox txtObjFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ OgreMeshConverter/frmMain.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mogre;
+
+namespace OgreMeshConverter
+{
+    public partial class frmMain : Form
+    {
+        private BackgroundWorker worker;
+
+        public frmMain()
+        {
+            InitializeComponent(); 
+
+            worker = new BackgroundWorker();
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+        }
+
+        private void btnBrowseMesh_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Ogre3d Mesh|*.mesh";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtOgreMesh.Text = dialog.FileName;
+            }
+        }
+
+        private void btnBrowseObj_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Obj file|*.obj";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtObjFile.Text = dialog.FileName;
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(txtOgreMesh.Text))
+                return;
+
+            btnConvert.Enabled = false;
+            worker.RunWorkerAsync();
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnConvert.Enabled = true;
+            MessageBox.Show("Done!");
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Mogre.Root root = new Mogre.Root("", "", "MainForm.log");
+
+            root.LoadPlugin("RenderSystem_Direct3D9");
+            root.LoadPlugin("Plugin_OctreeSceneManager");
+            root.LoadPlugin("Plugin_CgProgramManager");
+
+            Mogre.RenderSystem renderSystem = root.GetRenderSystemByName("Direct3D9 Rendering Subsystem");
+            root.RenderSystem = renderSystem;
+
+            root.Initialise(false);
+
+            DirectoryInfo di = new DirectoryInfo(txtOgreMesh.Text);
+
+            ResourceGroupManager.Singleton.AddResourceLocation(di.Parent.FullName, "FileSystem", "General");
+            ResourceGroupManager.Singleton.InitialiseAllResourceGroups();
+
+            MeshPtr mesh = MeshManager.Singleton.Load(Path.GetFileName(txtOgreMesh.Text), "General");
+
+            MeshToObj(mesh, txtObjFile.Text);
+        }
+
+        private void MeshToObj(MeshPtr mesh, string outputFileName)
+        {
+            DirectoryInfo di = new DirectoryInfo(outputFileName);
+
+            List<Vector3> vData;
+            List<uint> iData;
+            readMeshVertexDataAndIndexData(mesh, out vData, out iData);
+
+            generateObjFile(vData, iData, outputFileName);
+        }
+
+        private void readMeshVertexDataAndIndexData(MeshPtr mesh, out List<Vector3> vData, out List<uint> iData)
+        {
+            StaticMeshData staticMeshData = new StaticMeshData(mesh);
+            vData = staticMeshData.Vertices.ToList();
+            iData = staticMeshData.Indices.ToList();
+        }
+
+        private void generateObjFile(List<Vector3> vData, List<uint> iData, string outputFileName)
+        {
+            if (File.Exists(outputFileName))
+                File.Delete(outputFileName);
+
+            var writer = File.CreateText(outputFileName);
+
+            for(int i = 0; i < vData.Count; i++)
+            {
+                var v = vData[i];
+                writer.WriteLine(string.Format("v {0} {1} {2}", v.x, v.y, v.z));
+            }
+
+            int iLength = iData.Count / 3;
+            for (int i = 0; i < iLength; i++)
+            {
+                uint i1 = iData[i * 3 + 0];
+                uint i2 = iData[i * 3 + 1];
+                uint i3 = iData[i * 3 + 2];
+
+                Vector3 v1 = vData[(int)i1];
+                Vector3 v2 = vData[(int)i2];
+                Vector3 v3 = vData[(int)i3];
+
+                writer.WriteLine(string.Format("f {0} {1} {2}", i1 + 1, i2 + 1, i3 + 1));
+            }
+
+            writer.Dispose();
+        }
+    }
+}
